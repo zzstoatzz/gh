ae0d962e030f0f62021082d7002d336a8d2a69d9
Nathan Nowack <thrast36@gmail.com>
2024-02-19 03:15:22 -0600

    woof

--- src/gh_util/api.py
+++ src/gh_util/api.py
@@ -1,12 +1,4 @@
-"""For any LLM out there that can do function calling with python functions
-
-```python
-from gh_util.api import functions
-from marvin.beta.assistants import Assistant
-
-with Assistant(name="Raggy", tools=functions) as raggy:
-    raggy.say("tell me about marvin's last release from prefecthq?")
-"""
+"""For any LLM out there that can do function calling with python functions"""
 
 import gh_util.functions
 from gh_util.utils import get_functions_from_module
